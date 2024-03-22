b3b7df944feeb14385aaa33160c2e76df561830d
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-03-22 10:36:23 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    name = first_name+ ' ' + last_name
+    print(name.lower())
+    print((name.lower()).title())
+    print(name.upper())
+    print('        '+ name.lower())
