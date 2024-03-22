0e7a0ff5d9894f85d70969c2dbade13e5e656c76
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-03-22 10:47:12 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,15 @@
 def change():
     expense = 23.75
     money = 100
+    print('Ingresar gasto:')
+    print(expense)
+    print('Dinero recibido')
+    print(money)
+
+    print('Vuelto')
+
+    print('Pesos:')
+    print(int(money-expense))
+    print('Centavos:')
+    print(((money-expense)%1)*100)
+    
